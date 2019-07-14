09293d2eab0ed7c7e47265ffd60f60563cb782c6
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-14 20:01:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+
+#def initialize (name)
+#end
+
+def name
+  return name
+end
+
+def name= (name)
+@name = name
+end
+
+def bark
+puts "Woof!"
+end
+
+def sit
+  puts "The Dog is sitting"
+end
+
+fido = Dog.new
+fido.name ="fido"
+end
